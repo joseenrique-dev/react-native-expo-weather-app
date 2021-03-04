b1eb8a990eec4eacba6618d5fa03c9b24ef02733
José Enrique <otipip90@gmail.com>
2021-03-04 14:57:07 -0500

    update app and resolve error packages.

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+// import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from "expo-location";
+
 
 export default function App() {
+  const [ errorMessage, setErrorMessage ] = useState(null);
+
+  useEffect(() => {
+    
+    load();
+    
+  }, [])
+
+  async function load(){
+    try {
+      //ask for permissions.
+      let { status } = await Location.requestPermissionsAsync();
+      
+      if( status !== 'granted' ){
+        
+        setErrorMessage('Access to location is nedded to run the App !!!');
+        return;
+      }
+      const locationAsync = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+
+       const { latitude, longitude } = locationAsync.coords;
+      
+       alert(`My locations are: Longitud: ${longitude} and latitud: ${latitude}`);
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>My first ReactNative App Yeaa Baby!!!...</Text>
+      
     </View>
   );
 }
@@ -17,5 +48,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    fontSize:20
   },
 });
